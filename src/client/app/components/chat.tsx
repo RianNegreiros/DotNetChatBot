b88ae35f80e0f8daaf309548d545905198c8f46c
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-02-07 15:46:28 -0300

    fix: send message on enter
    change the textarea element to input

--- src/client/app/components/chat.tsx
+++ src/client/app/components/chat.tsx
@@ -53,12 +53,11 @@ export default function Chat() {
       <form onSubmit={handleSubmit}>
         <label htmlFor="chat" className="sr-only">Your message</label>
         <div className="flex items-center px-3 py-2 rounded-lg bg-gray-50 dark:bg-gray-700">
-          <textarea
+          <input
             id="chat"
-            rows={1}
             className="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="Your message..." value={text} onChange={(e) => setText(e.target.value)}
-          ></textarea>
+          />
           <button type="submit" className="inline-flex justify-center p-2 text-blue-600 rounded-full cursor-pointer hover:bg-blue-100 dark:text-blue-500 dark:hover:bg-gray-600">
             <svg className="w-5 h-5 rotate-90 rtl:-rotate-90" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
               <path d="m17.914 18.594-8-18a1 1 0 0 0-1.828 0l-8 18a1 1 0 0 0 1.157 1.376L8 18.281V9a1 1 0 0 1 2 0v9.281l6.758 1.689a1 1 0 0 0 1.156-1.376Z" />
