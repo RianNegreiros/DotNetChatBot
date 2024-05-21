0dc2b8616a0e66bdd74b98635e6174c4808bd052
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2024-05-21 11:55:41 -0300

    refactor: remove loading component border a make wide as the chat component

--- src/client/app/components/loading.tsx
+++ src/client/app/components/loading.tsx
@@ -2,34 +2,34 @@ export default function Loading() {
   return (
     <div
       role='status'
-      className='mb-4 max-w-lg space-y-2.5 rounded-lg border border-gray-200 p-6 dark:border-gray-700 dark:bg-gray-900'
+      className='mb-4 space-y-2.5 p-6 dark:bg-gray-900'
     >
       <div className='flex w-full items-center'>
         <div className='h-2.5 w-32 animate-pulse-slow rounded-full bg-gray-200 dark:bg-gray-700'></div>
         <div className='ms-2 h-2.5 w-24 animate-pulse-normal rounded-full bg-gray-300 dark:bg-gray-600'></div>
         <div className='ms-2 h-2.5 w-full animate-pulse-fast rounded-full bg-gray-300 dark:bg-gray-600'></div>
       </div>
-      <div className='flex w-full max-w-[480px] items-center'>
+      <div className='flex w-full items-center'>
         <div className='h-2.5 w-full animate-pulse-normal rounded-full bg-gray-200 dark:bg-gray-700'></div>
         <div className='ms-2 h-2.5 w-full animate-pulse-slow rounded-full bg-gray-300 dark:bg-gray-600'></div>
         <div className='ms-2 h-2.5 w-24 animate-pulse-fast rounded-full bg-gray-300 dark:bg-gray-600'></div>
       </div>
-      <div className='flex w-full max-w-[400px] items-center'>
+      <div className='flex w-full items-center'>
         <div className='h-2.5 w-full animate-pulse-fast rounded-full bg-gray-300 dark:bg-gray-600'></div>
         <div className='ms-2 h-2.5 w-80 animate-pulse-normal rounded-full bg-gray-200 dark:bg-gray-700'></div>
         <div className='ms-2 h-2.5 w-full animate-pulse-slow rounded-full bg-gray-300 dark:bg-gray-600'></div>
       </div>
-      <div className='flex w-full max-w-[480px] items-center'>
+      <div className='flex w-full items-center'>
         <div className='ms-2 h-2.5 w-full animate-pulse-slow rounded-full bg-gray-200 dark:bg-gray-700'></div>
         <div className='ms-2 h-2.5 w-full animate-pulse-normal rounded-full bg-gray-300 dark:bg-gray-600'></div>
         <div className='ms-2 h-2.5 w-24 animate-pulse-fast rounded-full bg-gray-300 dark:bg-gray-600'></div>
       </div>
-      <div className='flex w-full max-w-[440px] items-center'>
+      <div className='flex w-full  items-center'>
         <div className='ms-2 h-2.5 w-32 animate-pulse-normal rounded-full bg-gray-300 dark:bg-gray-600'></div>
         <div className='ms-2 h-2.5 w-24 animate-pulse-slow rounded-full bg-gray-300 dark:bg-gray-600'></div>
         <div className='ms-2 h-2.5 w-full animate-pulse-fast rounded-full bg-gray-200 dark:bg-gray-700'></div>
       </div>
-      <div className='flex w-full max-w-[360px] items-center'>
+      <div className='flex w-full items-center'>
         <div className='ms-2 h-2.5 w-full animate-pulse-fast rounded-full bg-gray-300 dark:bg-gray-600'></div>
         <div className='ms-2 h-2.5 w-80 animate-pulse-normal rounded-full bg-gray-200 dark:bg-gray-700'></div>
         <div className='ms-2 h-2.5 w-full animate-pulse-slow rounded-full bg-gray-300 dark:bg-gray-600'></div>
